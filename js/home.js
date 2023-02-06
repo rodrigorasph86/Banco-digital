5f7bfc2f50f94ab0e859e078bb8e95b48f5f7336
Rodrigo Sousa <rodrigorasph@gmail.com>
2023-02-05 23:03:15 -0300

    finalizando projeto

--- js/home.js
+++ js/home.js
@@ -1,11 +1,15 @@
 function entrar(){
 
-   saldo = ""//parseFloat(prompt("Informe seu valor de investimento inicial: "))
-  let opcao = ""
+   let saldo = parseFloat(prompt("Login efetuado com SUCESSO!"+
+   "\n\nPara a ativacao da sua conta  precimaos de um deposito inicial." +
+   "\nQual o valor do seu ivestimento inicial?"))
 
-  do {
+   let opcao = ""
+
+  
+   do {
     opcao = prompt(
-      "E muito bom ter voce no nosso time! " +
+      "Parabéns, sua conta foi liberada! Escolha uma das opcoes! \n" +
       "\n1. Depositar" + 
       "\n2. Tranferir" +
       "\n3. Saldo" +
@@ -13,10 +17,16 @@ function entrar(){
     )
     switch (opcao) {
       case "1":
-        saldo += parseFloat(prompt("Informe o valor de deposito: "))
-        break;
+           saldo += parseFloat(prompt("Informe o valor a ser depositado"))
+           break;
       case "2":
-        saldo -= parseFloat(prompt("Informe o valor a ser removido:"))
+        valorTranferencia = parseFloat(prompt("Informe o valor a ser Transeferido:"))
+        if(valorTranferencia < saldo){
+         saldo =  saldo - valorTranferencia
+        }else{
+          alert("Seu saldo é inferior ao solicitado\n " + "No momento você tem disponível: " + saldo)
+        }
+          
         break;
       case "3":
         alert("Seu saldo é "+ saldo);
@@ -24,15 +34,34 @@ function entrar(){
       case "4":
         alert("Saindo")
         break  
-      default:
-        alert("Opcao invalida")
+      default:/*  */
+         alert("Opcao invalida")
         break;
     }
     
-  } while (opcao !== "4");
+  } while (opcao !== "4"); 
 
-}
-
-function abrirConta(){
   
+ }
+
+ function abrirConta(){
+  
+  const cliente = prompt("-- ABRA SUA CONTA --\nFicamos muito felizes no seu intesse! Siga as instrucoes." +
+ "\n\nDigite seu nome completo:")
+  const dia = parseInt(prompt("Digite qual o dia do seu aniversario: "))
+  const mes = parseInt(prompt("Digite o mês do seu aniversário: "))
+  const  ano  = parseInt(prompt("Agora digite o ano do seu nascimento: "))
+  let idade = 2023 - ano
+  if (idade > 18){
+    alert("Legal, " + cliente + "!" + "\nVocê ja tem idade para abrir uma conta em nosso banco! " + "\n" +
+    "\nSiga os proximos passos...")
+  }else{
+    alert("Que pena! Você ainda nao tem idade suciente para abrir a conta")
+  }
+  const cpf = parseFloat(prompt("Digite seu CPF: "))
+  const senha = parseInt(prompt("Digite uma senha com: 6 digitos XXXXXX"))
+
+
+  alert(cliente + " seu CPF: " + cpf + " foi APROVADO! Acesse sua conta com CPF e SENHA!")
 }
+
